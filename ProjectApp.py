4dcd313f6d005a8fb3fa6b542e3fc7975f63b3d5
Tess Chandler <stc89@georgetown.edu>
2024-12-08 19:19:25 -0500

    Updated App

--- ProjectApp.py
+++ ProjectApp.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import numpy as np
-import altair as alt
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
@@ -38,7 +37,7 @@ ss = pd.DataFrame({
     #age (numeric, above 98 considered missing)
     "age":np.where(s["age"] > 98, np.nan, s["age"])}).dropna(ignore_index=True)
 
-ss["sm_li"] = ss["sm_li"].astype(int).astype("category")
+ss["sm_li"] = ss["sm_li"].astype(int)
 ss["income"] = ss["income"].astype(int)
 ss["educ"] = ss["educ"].astype(int)
 ss["age"] = ss["age"].astype(int)
@@ -74,29 +73,28 @@ def create_person(income, educ, age, gender, par, marital):
     })
     return person
 
+def linkedin_class(income, educ, age, gender, par, marital):
+
+    # Load packages
+    from sklearn.linear_model import LogisticRegression
+    import plotly.graph_objects as go # data visualization package
+
+
+    #create person
+    person = create_person(income, educ, age, gender, par, marital)
+
+    # Make predictions using the logistic regression model
+    predicted_class = lr.predict(person)[0]
+    probs = lr.predict_proba(person)[0][1]
+
+    if predicted_class == 0:
+        return("Not a LinkedIn User")
+    else: 
+        return("LinkedIn User")
+    return(predicted_class)
+
 def linkedin_app(income, educ, age, gender, par, marital):
 
-    #warning messages if numbers are out of range
-    if income > 9 or income == 0 or educ > 6 or educ == 0 or age < 18 or age > 97 or gender > 1 or par > 1 or marital > 1:
-        st.write("## WARNING")
-    if income > 9 or income == 0:
-        st.write("Please enter a valid income (integer between 1 and 9)")
-    if educ > 6 or educ == 0:
-        st.write("Please enter a valid education level (integer between 1 and 6)")
-    if age < 18 or age > 97:
-        st.write("Please enter a valid age (integer between 18 and 97)") #using 18 because that is the lowest age in df
-    if gender > 1:
-        st.write("Please enter a valid gender (0 or 1)")
-    if par > 1:
-        st.write("Please enter a valid parental status (0 or 1)")
-    if marital > 1:
-        st.write("Please enter a valid marital status (0 or 1)")   
-
-    #fig will not print if any vars are out of range
-    if income > 9 or income == 0 or educ > 6 or educ == 0 or age < 18 or age > 97 or gender > 1 or par > 1 or marital > 1:
-        return
-
- 
     # Load packages
     from sklearn.linear_model import LogisticRegression
     import plotly.graph_objects as go # data visualization package
@@ -127,33 +125,54 @@ def linkedin_app(income, educ, age, gender, par, marital):
     # Plot likelihood on gauge plot
     fig = go.Figure(go.Indicator(
        mode = "gauge+number",
-       value = probs,
-       title = {'text': f"Probability of Using LinkedIn: {label}<br>Predicted Class: {class_label}"},
-       gauge = {"axis": {"range": [0, 1]},
+       value = probs *100,
+       title = {'text': f"<b>Probability of Using LinkedIn: {label}</b> <br> <i>Probabilities as Percentages</i>"},
+       gauge = {"axis": {"range": [0, 100],
+                "tickvals": [0, 25, 50, 75, 100]},
                     "steps": [
-                        {"range": [0, 0.25], "color":"red"},
-                        {"range": [0.25, 0.5], "color":"lightcoral"},
-                        {"range": [0.5, 0.75], "color":"lightgreen"},
-                        {"range": [0.75, 1], "color":"green"}],
-                "bar":{"color":"grey"}}))
+                        {"range": [0, 25], "color":"white"},
+                        {"range": [25, 50], "color":"lightblue"},
+                        {"range": [50, 75], "color":"royalblue"},
+                        {"range": [75, 100], "color":"darkblue"}],
+                "bar":{"color":"indianred"}}))
     return st.plotly_chart(fig)
 
 
+###UI for streamlit app
+
 st.markdown("##### Enter a person's characteristics to see their likelihood of using LinkedIn!")
-st.markdown("###### Definitions for each variable are at the bottom of the page")
+st.markdown("***Definitions for each variable are at the bottom of the page***")
+st.markdown("---")
+col1, col2, col3 = st.columns(3)
+with col1:
+    Income = st.selectbox("Select an Income Level:", [1, 2, 3, 4, 5, 6, 7, 8, 9])
+with col2:
+    Education = st.selectbox("Select an Education Level:", [1, 2, 3, 4, 5, 6])
+with col3:
+    Age = st.slider("Select an Age:", 18,97)
 
-Income = st.number_input("Enter Income:", value=5)
-Education = st.number_input("Enter Education Level:", value=5)
-Age = st.number_input("Enter Age:", value=30)
-Gender = st.number_input("Enter Gender:", value=1)
-Parent = st.number_input("Enter Parental Status:", value=0)
-Married = st.number_input("Enter Marital Status:", value=0)
+col4, col5, col6 = st.columns(3)
+with col4:
+    Gender = st.selectbox("Select a Gender:", [0,1])
+with col5:
+    Parent = st.selectbox("Select a Parental Status:", [0,1])
+with col6:
+    Married = st.selectbox("Select a Marital Status:", [0,1])
 
+st.markdown("---")
+st.markdown(f"<p style='color:indianred; font-weight:bold;'>This person's predicted class is: {linkedin_class(Income, Education, Age, Gender, Parent, Married)}</p>", unsafe_allow_html=True)
 linkedin_app(Income, Education, Age, Gender, Parent, Married)
 
+st.markdown("---")
 st.markdown("### Variable Key")
-st.markdown("##### Income")
-st.markdown("""
+
+col7, col8, col9 = st.columns(3)
+col7.markdown("##### Income")
+col8.markdown("##### Education")
+col9.markdown("##### Age")
+
+col10, col11, col12 = st.columns(3)
+col10.markdown("""
 - 1: Less than 10,000
 - 2: 10,000 - 20,000
 - 3: 20,000 - 30,000
@@ -164,8 +183,7 @@ st.markdown("""
 - 8: 100,000 - 150,000
 - 9: 150,000 and above
 """)
-st.markdown("##### Education")
-st.markdown("""
+col11.markdown("""
 - 1: Less than highschool
 - 2: Highschool incomplete
 - 3: Highschool graduate
@@ -173,23 +191,28 @@ st.markdown("""
 - 5: Two-year associate degree from college or university
 - 6: Four-year college or university degree
 """)
-st.markdown("##### Age")
-st.markdown("""
+col12.markdown("""
 - 18 through 96: Numeric Age
 - 97: 97 years or above
 """)
-st.markdown("##### Gender")
-st.markdown("""
+
+col13, col14, col15 = st.columns(3)
+col13.markdown("##### Gender")
+col14.markdown("##### Parental Status")
+col15.markdown("##### Marital Status")
+
+col16, col17, col18 = st.columns(3)
+col16.markdown("""
 - 0: Not Female (Male or Other)
 - 1: Female
 """)
-st.markdown("##### Parental Status")
-st.markdown("""
+
+col17.markdown("""
 - 0: Not a Parent
 - 1: Parent
 """)
-st.markdown("##### Marital Status")
-st.markdown("""
+
+col18.markdown("""
 - 0: Not Married (Living with partner, divorced, separated, widowed, never married)
 - 1: Married
 """)
